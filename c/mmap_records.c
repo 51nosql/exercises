06f5ca5e2cbff3dd8dd7c2343524f9d04be5d1e0
51nosql <51nosql@gmail.com>
2013-08-19 13:47:14 +0800

    mmap

--- c/mmap_records.c
+++ c/mmap_records.c
@@ -13,5 +13,39 @@ typedef struct {
 
 int main()
 {
+	RECORD record, *mapped;
+	int i, f;
+	FILE *fp;
+
+	fp = fopen("mmap_records.dat", "w+");
+	for(i=0; i<NRECORDS; i++) {
+		record.integer = i;
+		sprintf(record.string, "RECORD-%d", i);
+		fwrite(&record, sizeof(record), 1, fp);
+	}
+	fclose(fp);
+
+	fp = fopen("mmap_records.dat", "r+");
+	fseek(fp, 43*sizeof(record), SEEK_SET);
+	fread(&record, sizeof(record), 1, fp);
+
+	record.integer = 143;
+	sprintf(record.string, "RECORD-%d", record.integer);
+
+	fseek(fp, 43*sizeof(record), SEEK_SET);
+	fwrite(&record, sizeof(record), 1, fp);
+	fclose(fp);
+
+	f = open("mmap_records.dat", O_RDWR);
+	mapped = (RECORD *)mmap(0, NRECORDS*sizeof(record),
+							PROT_READ|PROT_WRITE, MAP_SHARED, f, 0);
+
+	mapped[43].integer = 243;
+	sprintf(mapped[43].string, "RECORD-%d", mapped[43].integer);
+
+	msync((void *)mapped, NRECORDS*sizeof(record), MS_ASYNC);
+	munmap((void *)mapped, NRECORDS*sizeof(record));
+	close(f);
+
 	exit(0);
 }
