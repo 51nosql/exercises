83f079739421d406badc4f38d7cf0f0ef6e7f51c
51nosql <51nosql@gmail.com>
2013-09-25 14:01:58 +0800

    gdb debug

---
+++ c/debug/debug3.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+// gcc -g
+// gdb a.out
+// (gdb) run
+// (gdb) backtrace
+// (gdb) print j
+// (gdb) print a[3]
+// (gdb) print j
+// (gdb) print a[$-1].key
+// (gdb) print a[$$-1].key
+// (gdb) list
+
+
+typedef struct {
+	char data[4096];
+	int key;
+} item;
+
+item array[] = {
+	{"bill", 3},
+	{"neil", 4},
+	{"john", 2},
+	{"rick", 5},
+	{"alex", 1},
+};
+
+sort(a, n)
+item *a;
+{
+	int i = 0, j = 0;
+	int s = 1;
+
+	for(; i < n && s != 0; i++) {
+		s = 0;
+		for(j = 0; j < n; j++) {
+			if(a[j].key > a[j+1].key) {
+				item t = a[j];
+				a[j] = a[j+1];
+				a[j+1] = t;
+				s++;
+			}
+		}
+		n--;
+	}
+}
+
+int main()
+{
+	int i;
+	sort(array, 5);
+	for(i = 0; i < 5; i++)
+		printf("array[%d] = {%s, %d}\n",
+				i, array[i].data, array[i].key);
+	return 0;	
+}
+
